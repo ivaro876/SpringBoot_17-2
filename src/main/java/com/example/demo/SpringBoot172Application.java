8e444af4ab757579aa425186a2251fe7a69fec11
ivaro876 <novaro456@gmail.com>
2018-11-23 11:22:27 -0800

    Initial commit

---
+++ src/main/java/com/example/demo/SpringBoot172Application.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBoot172Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBoot172Application.class, args);
+    }
+}
